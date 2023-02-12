c6246c013b256fa1a415dfc5ffc327afe729af78
success ibekwe <Successibekwe047@gmail.com>
2023-02-12 02:53:00 +0100

    added pop uo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+// import PopUp from "./components/PopUp";
 
 function App() {
   return (
     <>
       <Header />
+      {/* <PopUp /> */}
       <Main />
     </>
   );
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -5,15 +5,15 @@ const PopUp = () => {
   return (
     <>
       <div className="w-full md:w-[446px] h-[475px] bg-[#15141B] rounded-[20px]">
-        <div className="w-full h-full flex flex-col justify-center items-center gap-4 text-center text-white">
+        <div className="w-full h-full flex flex-col justify-center items-center gap-4  text-white">
           <div>
             <img src={check} alt="" />
           </div>
-          <div>
-            <h4 className="text-white font-[700] text-[]24px leading-[32.4px]">
+          <div className="w-[80%] mx-auto">
+            <h4 className="text-white font-[700] text-[]24px leading-[32.4px] text-center">
               Congratulations
             </h4>
-            <p className="text-[16px] leading-[21.6px] text-white/90">
+            <p className="text-[16px] leading-[21.6px] text-white/90 text-center">
               Great move! You're one step closer to getting your hands on the
               product. Please check your mail for more information.
             </p>
